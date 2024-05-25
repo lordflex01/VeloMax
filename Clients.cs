98f1104b379f5539dba93a420dd94e7d77664a38
Xela-P <alexandre.firmin2@gmail.com>
2024-05-25 16:37:24 +0200

    Ajout de la connexion a la base et du formulaire client

--- Clients.cs
+++ Clients.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Data;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace VeloMax
 {
-    internal class Clients
+    public partial class Clients :Form
     {
+        public Clients()
+        {
+            InitializeCompenent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SqlConnection con = new SqlConnection("Data Source=NiluNilesh;Integrated Security=True");  
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True;User Instance=True");
+            SqlCommand cmd = new SqlCommand("sp_insert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@nom", textBox1.Text);
+            cmd.Parameters.AddWithValue("@prenom", textBox2.Text);
+            cmd.Parameters.AddWithValue("@adresse", textBox3.Text);
+            cmd.Parameters.AddWithValue("@tel", textBox4.Text);
+            cmd.Parameters.AddWithValue("@CP", textBox5.Text);
+            cmd.Parameters.AddWithValue("@ville", textBox6.Text);
+            cmd.Parameters.AddWithValue("@tel", textBox7.Text);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            if (i != 0)
+            {
+                MessageBox.Show(i + "Data Saved");
+            }
+
+
+
+
+        }
+
     }
 }
--- DBConnection.cs
+++ DBConnection.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
 
 namespace VeloMax
 {
     internal class DBConnection
     {
+        static void connexion() {
+            
+            string connectionString = "datasource=localhost;port=3306;username=root;password=;database=Velomax;";
+
+            string query = "SELECT * FROM user";
+
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                // Open the database
+                databaseConnection.Open();
+
+                // Execute the query
+                reader = commandDatabase.ExecuteReader();
+
+                // All succesfully executed, now do something
+
+                // IMPORTANT : 
+                // If your query returns result, use the following processor :
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // As our database, the array will contain : ID 0, FIRST_NAME 1,LAST_NAME 2, ADDRESS 3
+                        // Do something with every received database ROW
+                        string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3) };
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+
+                // Finally close the connection
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
     }
-}
+
+        
+    }
+
+
